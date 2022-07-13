ba74024c7ab61a1ca3f033c77eb6aa2e5cade850
himasai_1423 <bhsr2003@gmail.com>
2022-07-13 10:10:28 +0530

    created attachment button with uploader component

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router';
 import firebase from 'firebase/app';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -70,6 +71,7 @@ const Bottom = () => {
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal />
         <Input
           placeholder="Write a new message here..."
           value={input}
